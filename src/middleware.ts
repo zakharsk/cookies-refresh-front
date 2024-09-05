5db1a1425482c45993253cd821f5d1ea8790a034
Zakhar Skorokhodov <zaharsk@gmail.com>
2024-09-05 17:34:19 +0200

    feat(Middleware): Tuned requests handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,40 +3,78 @@ import { NextRequest, NextResponse } from 'next/server';
 import { accessTokenCookieName, refreshTokenCookieName } from '@/constants';
 import { parseCookie } from '@/lib';
 
-export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/_next')) {
-    return NextResponse.next();
+export async function middleware(originalRequest: NextRequest) {
+  const originalResponse = NextResponse.next({
+    request: originalRequest,
+  });
+
+  if (
+    originalRequest.nextUrl.pathname.match(
+      '\\/((api|_next\\/static|_next\\/image|favicon\\.ico|sitemap\\.xml|robots\\.txt).*)',
+    )
+  ) {
+    // console.log(
+    //   'Middleware:',
+    //   'Internal Next.js request. Returning the original response unchanged.',
+    //   originalRequest.nextUrl.pathname,
+    // );
+    return originalResponse;
   }
 
-  const accessTokenCookie = request.cookies.get(accessTokenCookieName);
-  const refreshTokenCookie = request.cookies.get(refreshTokenCookieName);
-  if (accessTokenCookie?.value && refreshTokenCookie?.value) {
-    return NextResponse.next();
+  const accessTokenCookie = originalRequest.cookies.get(accessTokenCookieName);
+  const refreshTokenCookie = originalRequest.cookies.get(
+    refreshTokenCookieName,
+  );
+
+  if (!refreshTokenCookie?.value) {
+    // console.log(
+    //   'Middleware:',
+    //   'There is no Refresh token in the request. Returning the original response unchanged.',
+    // );
+    return originalResponse;
   }
 
-  if (refreshTokenCookie?.value) {
-    console.log('middleware:', 'Refresh!');
+  if (!accessTokenCookie?.value) {
+    // console.log('Middleware:', 'The tokens need to be refreshed!');
     const apiHost = process.env.API_HOST;
+    const apiPrefix = process.env.API_URL_PREFIX || '';
     const apiVersion = process.env.API_DEFAULT_VERSION;
-    const requestUrl = `${apiHost}/v${apiVersion}/tokens`;
+    const refreshRequestUrl = `${apiHost}${apiPrefix}/v${apiVersion}/tokens`;
 
-    const response = NextResponse.next({ request: request });
-
-    const refreshRequest = new NextRequest(requestUrl, { method: 'PATCH' });
+    const refreshRequest = new NextRequest(refreshRequestUrl, {
+      method: 'PATCH',
+    });
     refreshRequest.cookies.set(
       refreshTokenCookieName,
       refreshTokenCookie.value,
     );
 
+    // console.log('Middleware:', 'Sending refresh request.');
     const refreshResponse = await fetch(refreshRequest);
-    refreshResponse.headers.getSetCookie().forEach((cookieString) => {
-      const cookie = parseCookie(cookieString);
-      if (cookie) {
-        response.cookies.set(cookie);
-      }
-    });
 
-    return response;
+    if (refreshResponse.status === 200) {
+      refreshResponse.headers.getSetCookie().forEach((cookieString) => {
+        const cookie = parseCookie(cookieString);
+        if (cookie) {
+          // console.log(
+          //   'Middleware:',
+          //   `Attaching ${cookie.name} cookie to the original response.`,
+          // );
+          originalResponse.cookies.set(cookie);
+        }
+      });
+    } else if (refreshResponse.status === 401) {
+      // console.log(
+      //   'Middleware:',
+      //   'Invalid RefreshToken. Deleting RefreshToken cookie.',
+      // );
+      originalResponse.cookies.delete(refreshTokenCookieName);
+    } else {
+      console.log(
+        'Middleware:',
+        `An unexpected response to refresh request: ${refreshResponse.status}`,
+      );
+    }
   }
-  return NextResponse.next();
+  return originalResponse;
 }
